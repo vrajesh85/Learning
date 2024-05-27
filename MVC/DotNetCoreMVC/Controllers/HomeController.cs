24b78a5d19cb3b989ac8688a2ea1142a57615de9
Vemulakonda Rajesh <Vemulakonda.rajesh@optum.com>
2024-05-27 12:51:24 +0530

    Pushing further changes 27 May

---
+++ MVC/DotNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using DotNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private List<SelectListItem> lstStates = new List<SelectListItem>();
+         
+        
+        private Person[] personData = {
+            new Person {PersonId = 1, FirstName = "Adam", LastName = "Freeman",
+                Role = Role.Admin},
+            new Person {PersonId = 2, FirstName = "Jacqui", LastName = "Griffyth",
+                Role = Role.User},
+            new Person {PersonId = 3, FirstName = "John", LastName = "Smith",
+                Role = Role.User},
+            new Person {PersonId = 4, FirstName = "Anne", LastName = "Jones",
+                Role = Role.Guest}
+        };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int id = 1)
+        {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+            dataItem.Country = GetCountry();
+            return View("Index",dataItem);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Person values)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy(string name="Rajesh")
+        {
+            // return RedirectToAction("Index");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        private List<SelectListItem> GetStates(string country)
+        {
+            var lstStates = new List<SelectListItem>();
+
+            if(country == "US")
+            {
+                lstStates.Add(new SelectListItem("TG", "1"));
+                lstStates.Add(new SelectListItem("AP", "2"));
+                lstStates.Add(new SelectListItem("TN", "3"));
+            }
+            else
+            {
+                lstStates.Add(new SelectListItem("TX", "4"));
+                lstStates.Add(new SelectListItem("AZ", "5"));
+            }
+
+            return lstStates;
+        }
+
+        private List<SelectListItem> GetCountry()
+        {
+            var lstCountries = new List<SelectListItem>();
+
+            lstCountries.Add(new SelectListItem("IN", "1"));
+            lstCountries.Add(new SelectListItem("US", "2"));
+
+            return lstCountries;
+        }
+    }
+}
---
+++ MVC/DotNetCoreMVC/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreMVC.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Address HomeAddress { get; set; }
+        public bool IsApproved { get; set; }
+        public Role Role { get; set; }
+        public List<SelectListItem> State { get; set; }
+        public List<SelectListItem> Country { get; set; }
+    }
+
+    public class Address
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User,
+        Guest
+    }
+}
